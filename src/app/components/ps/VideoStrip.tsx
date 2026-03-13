385dc10311cf095debe6eafc1dcdcdf13ad251ea
Jvictorio09 <juliavictorio16@gmail.com>
2026-03-13 19:41:06 +0800

    udpated

--- src/app/components/ps/VideoStrip.tsx
+++ src/app/components/ps/VideoStrip.tsx
@@ -1,12 +1,14 @@
+const YOUTUBE_VIDEO_ID = "me-jbmDgSfA";
+
 export function VideoStrip() {
   return (
     <div className="video-strip">
       <iframe
-        src="https://player.vimeo.com/video/1169222061?badge=0&autopause=0&player_id=0&app_id=58479&background=1&autoplay=1&loop=1&muted=1"
+        src={`https://www.youtube.com/embed/${YOUTUBE_VIDEO_ID}?autoplay=1&loop=1&playlist=${YOUTUBE_VIDEO_ID}&mute=1&controls=0&showinfo=0&rel=0&modestbranding=1&playsinline=1`}
         frameBorder="0"
-        allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
-        title="GettyImages-1359775000"
+        allow="autoplay; fullscreen; picture-in-picture; encrypted-media"
+        allowFullScreen
+        title="PlayerScope"
       />
     </div>
   );
